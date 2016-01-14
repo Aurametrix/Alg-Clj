1f4c90da5fbc0f3b500c75af735f96803d009d60
Aurametrix <irene@aurametrix.com>
2016-01-13 20:57:47 -0800

    recursive, but might blow the stack as p approaches 0.5

--- PROBLEMS/step.clj
+++ PROBLEMS/step.clj
@@ -1,8 +1,8 @@
-(defn step-up1
-  "Straightforward implementation: keep track of how many level we
-   need to ascend, and stop when this count is zero."
+(defn step-up2
+  "Non-tail-recursive. No numbers."
   []
-  (loop [deficit 1]
-    (or (zero? deficit)
-	(recur (if (step) (dec deficit)
-		   (inc deficit)))) ) ) )
+  (if (not (step))
+    (do (step-up2) ;; undo the fall
+	(step-up2) ;; try again
+	)
+    true))
